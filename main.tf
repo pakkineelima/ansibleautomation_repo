6a640c2d868309379d4a84534888c83d3d2a20b8
pakkineelima <112457594+pakkineelima@users.noreply.github.com>
2022-11-06 16:31:47 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+resource "aws_instance" "autoamtion_ins" {
+  ami           = "ami-089a545a9ed9893b6"
+  instance_type = "t2.micro"
+  key_name      = "myfirstkeypair"
+  count         = "1"
+}
